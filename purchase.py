9d3265cfcf9846c99273ca33c0c41b8b07a3d97f
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2016-08-23 14:15:00 -0600

    cambios a picking y el de compras

--- purchase.py
+++ purchase.py
@@ -5,5 +5,5 @@ import logging
 class purchase_order(osv.osv):
     _inherit = "purchase.order"
     _columns = {
-        'departamento_id': fields.many2one('hr.department', 'Departamento'),
+        'departamento_id': fields.many2one('hr.department', 'Departamento', ondelete='restrict'),
     }
