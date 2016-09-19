8c722cabc4988b5c40367db2036ef80bb2bfe3db
jefferson Silva <jeff93leo@users.noreply.github.com>
2016-09-19 12:39:22 -0600

    Agregando Campo a Bank Statement

--- account.py
+++ account.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from openerp.osv import osv, fields
+from openerp import models
 
 class account_voucher(osv.osv):
     _inherit = 'account.voucher'
@@ -8,3 +9,16 @@ class account_voucher(osv.osv):
     _columns = {
         'tipo': fields.selection([ ('no negociable', 'No negociable'), ('negociable', 'Negociable'), ], 'Tipo')
     }
+
+# Creacion Campo y metodo onchange para asignar usuario a un grupo determinado en Bank statements  
+class account_bank_statement_distefano(models.Model):
+    _inherit='account.bank.statement'
+    _columns= {
+        'pos_config_id': fields.many2one('pos.config', 'Tienda',required=True),
+    }
+
+    def onchange_bank_statement_grupo(self,cr,uid,ids,context=None):
+        vals = {}
+    	objeto=self.pool.get("res.users").browse(cr,uid,uid)
+    	vals.update({'pos_config_id': objeto.pos_config})
+        return {'value': vals}
